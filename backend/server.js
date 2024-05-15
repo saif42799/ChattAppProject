c3b49b9b67eb92ea94a326d46e36283b8831bacb
Saif <sshaikh4@ggc.edu>
2024-05-15 10:32:05 -0400

    first commit of Chatt App project. Setup up isnt complete but I decide to push this because of the amonut of changes.

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// express server
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    // root route http://localhost:5000/
+    res.send("Hello Worlds");
+
+});
+
+app.listen(5000, () => console.log("Server Running on port 5000"));
