5048c11ba296bbec0b7c8b0dc628ccc751995b67
Saif <sshaikh4@ggc.edu>
2024-05-18 00:47:26 -0400

    pc changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 // express server
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
     // root route http://localhost:5000/
-    res.send("Hello Worlds");
-
+    res.send("Hello World!!");
 });
 
-app.listen(5000, () => console.log("Server Running on port 5000"));
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
